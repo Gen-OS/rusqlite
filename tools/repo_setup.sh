90327b1ce89730460fae0a9df4c986d0f292bb9c
Sam Gammon <sam@elide.ventures>
2024-09-12 16:07:19 -0700

    fix: double genos repo detection
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- tools/repo_setup.sh
+++ tools/repo_setup.sh
@@ -5,13 +5,15 @@ if [ -z "$GENOS_REPO_TOKEN" ]; then
     exit 1
 fi
 
-# check if the ~/.cargo/config file already contains the token `genos`
-if grep -q "genos" ~/.cargo/config; then
+echo "Setting up GenOS repo..."
+cat ~/.cargo/config.toml
+
+# check if the ~/.cargo/config.toml file already contains the token `genos`
+if grep -q "genos" ~/.cargo/config.toml; then
     echo "GenOS repo is already set up."
     exit 0
 fi
 
-echo "Setting up GenOS repo..."
 mkdir -p ~/.cargo
 echo "[registries.genos]" >> ~/.cargo/config.toml
 echo "token = \"Bearer $GENOS_REPO_TOKEN\"" >> ~/.cargo/config.toml
